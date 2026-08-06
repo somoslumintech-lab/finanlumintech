68287ddfc161af10acc58517165f5c4cc3f4d90b
somoslumintech-lab <somoslumintech@gmail.com>
2026-08-06 16:21:20 -0300

    chore: initialize Android project

---
+++ build.gradle.kts
@@ -0,0 +1,45 @@
+plugins {
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+    id("org.jetbrains.kotlin.plugin.compose")
+}
+
+android {
+    namespace = "com.financeapp"
+    compileSdk = 35
+
+    defaultConfig {
+        applicationId = "com.financeapp"
+        minSdk = 26
+        targetSdk = 35
+
+        versionCode = 1
+        versionName = "1.0"
+    }
+
+    buildFeatures {
+        compose = true
+    }
+
+    packaging {
+        resources {
+            excludes += "/META-INF/{AL2.0,LGPL2.1}"
+        }
+    }
+}
+
+dependencies {
+
+    implementation("androidx.core:core-ktx:1.15.0")
+
+    implementation("androidx.activity:activity-compose:1.10.1")
+
+    implementation("androidx.compose.ui:ui:1.7.8")
+    implementation("androidx.compose.ui:ui-tooling-preview:1.7.8")
+
+    implementation("androidx.compose.material3:material3:1.3.1")
+
+    debugImplementation(
+        "androidx.compose.ui:ui-tooling:1.7.8"
+    )
+}
