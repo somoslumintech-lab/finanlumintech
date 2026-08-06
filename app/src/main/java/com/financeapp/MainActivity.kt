fde47484b809de22e1fbbbe24c28bee0b08222c9
somoslumintech-lab <somoslumintech@gmail.com>
2026-08-06 17:11:15 -0300

    Add files via upload

--- app/src/main/java/com/financeapp/MainActivity.kt
+++ app/src/main/java/com/financeapp/MainActivity.kt
@@ -1,1 +1,44 @@
+package com.financeapp
 
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+
+class MainActivity : ComponentActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContent {
+            FinanceApp()
+        }
+    }
+}
+
+@Composable
+fun FinanceApp() {
+    MaterialTheme {
+        Column(
+            modifier = Modifier.fillMaxSize(),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
+        ) {
+            Text(
+                text = "Finance",
+                style = MaterialTheme.typography.headlineLarge
+            )
+
+            Text(
+                text = "Seu dinheiro, automaticamente."
+            )
+        }
+    }
+}
