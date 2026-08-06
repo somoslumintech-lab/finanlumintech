7470ae78fb4ceb82bedcc718ca0f236052b2b41e
somoslumintech-lab <somoslumintech@gmail.com>
2026-08-06 19:23:36 -0300

    feat: add dashboard UI dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -38,7 +38,9 @@ dependencies {
     implementation("androidx.compose.ui:ui-tooling-preview:1.7.8")
 
     implementation("androidx.compose.material3:material3:1.3.1")
-
+    
+    implementation("androidx.compose.material:material-icons-extended:1.7.8")
+    
     debugImplementation(
         "androidx.compose.ui:ui-tooling:1.7.8"
     )
