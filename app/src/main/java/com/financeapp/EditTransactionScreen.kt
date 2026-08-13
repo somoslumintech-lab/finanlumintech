ee71ca07be885f3f62e441412f64b896916ce0a9
somoslumintech-lab <somoslumintech@gmail.com>
2026-08-13 09:38:36 -0300

    gradle assembleDebug --stacktrace

--- app/src/main/java/com/financeapp/EditTransactionScreen.kt
+++ app/src/main/java/com/financeapp/EditTransactionScreen.kt
@@ -14,6 +14,7 @@ import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material3.Button
 import androidx.compose.material3.DropdownMenu
 import androidx.compose.material3.DropdownMenuItem
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.OutlinedTextField
@@ -33,6 +34,7 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun EditTransactionScreen(
     bottomPadding: PaddingValues,
